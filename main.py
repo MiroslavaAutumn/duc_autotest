db49459d8e477f182519d306ad74478ee382d304
MiroslavaAutumn <miroslavaautumn@gmail.com>
2021-06-07 11:18:34 +0300

    added wait for pressing instead of time.sleep

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import unittest
 import time
 import options
@@ -15,30 +18,53 @@ class FirstStart(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_first_start_test_case(self):
+    def test_first_start_case(self):
+        print("Запуск!")
         driver = self.driver
         driver.get(url=url)
+        wait = WebDriverWait(driver, 100)
 
-        driver.find_element_by_xpath("//span").click()
-        time.sleep(1)
+        start_button = wait.until(EC.element_to_be_clickable((
+            By.XPATH,
+            "//span")))
+        start_button.click()
+        print("start button is ok")
+        time.sleep(0.5)
 
         for i in range(0, 3):
-            driver.find_element_by_xpath(f"//button[@id='checkbox-{i}-0']/span").click()
-            time.sleep(0.1)
+            checkbox = wait.until(EC.element_to_be_clickable((
+                By.XPATH,
+                f"//button[@id='checkbox-{i}-0']/span")))
+            checkbox.click()
+            print(f"chechbox {i} is ok")
 
-        driver.find_element_by_xpath("//div[2]/div/div/button/span").click()
-        time.sleep(1)
+        continue_button = wait.until(EC.element_to_be_clickable((
+            By.XPATH,
+            "//div[2]/div/div/button/span")))
+        continue_button.click()
+        print("continue button is ok")
+        time.sleep(0.5)
 
         for i in range(28, 42):
-            driver.find_element_by_xpath(f"//button[@id='toggle-{i}-0']/span").click()
-            time.sleep(0.1)
+            select_coin = wait.until(EC.element_to_be_clickable((
+                By.XPATH,
+                f"//button[@id='toggle-{i}-0']/span")))
+            select_coin.click()
+            print(f"toggle {i} is ok")
 
-        driver.find_element_by_xpath("//div/button/span").click()
-        time.sleep(15)
-        driver.find_element_by_xpath("//div[3]/button/span").click()
-        time.sleep(1)
-        driver.find_element_by_xpath("//div[3]/button[2]/span").click()
-        time.sleep(10)
+        wallet_button = "//div/button/span"
+        no_password = "//div[3]/button/span"
+        no_password_2 = "//div[3]/button[2]/span"
 
+        create_wallet = [wallet_button, no_password, no_password_2]
+        for button in create_wallet:
+            start_creating = wait.until(EC.element_to_be_clickable((
+                By.XPATH,
+                button)))
+            start_creating.click()
+            time.sleep(1)
+            print(f"{button} is ok")
+
+        time.sleep(60)
         driver.close()
         driver.quit()
