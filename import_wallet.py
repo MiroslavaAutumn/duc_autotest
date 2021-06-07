ace29f521a3af0b4a38a7555e9e57c585331d7a5
MiroslavaAutumn <miroslavaautumn@gmail.com>
2021-06-07 13:36:58 +0300

    add import from menu script

--- import_wallet.py
+++ import_wallet.py
@@ -72,8 +72,40 @@ class First_start_import_wallet(unittest.TestCase):
                 By.XPATH,
                 button)))
             ok.click()
+            time.sleep(2)
+            print("refuse password is ok")
+
+    ### import from menu ###
+        import_from_menu = wait.until(EC.element_to_be_clickable((
+            By.XPATH,
+            "//ion-tab[@id='tabpanel-t1-1']/page-wallets/ion-header/ion-navbar/ion-buttons[2]/button")))
+        import_from_menu.click()
+        print("start import2 is ok")
+
+        driver.find_element_by_xpath("//div[2]/ion-list/button/div/div").click()
+        driver.find_element_by_xpath("//div[2]/div/div/ion-list/button[4]/div/div").click()
+        driver.find_element_by_xpath("//textarea").click()
+        driver.find_element_by_xpath("//textarea").clear()
+        driver.find_element_by_xpath("//textarea").send_keys(private_keys.wallet_key_2)
+        print("key is ok")
+
+        import_wallet = wait.until(EC.element_to_be_clickable((
+            By.XPATH,
+            "//page-import-wallet/ion-header/ion-navbar/ion-buttons/button")))
+        import_wallet.click()
+        print("import key is ok")
+
+        no_password = "//div[3]/button/span"
+        no_password_2 = "//div[3]/button[2]/span"
+        refuse_password = [no_password, no_password_2]
+        for button in refuse_password:
+            ok = wait.until(EC.element_to_be_clickable((
+                By.XPATH,
+                button)))
+            ok.click()
             time.sleep(1)
             print("refuse password is ok")
+
 
         time.sleep(60)
         driver.close()
