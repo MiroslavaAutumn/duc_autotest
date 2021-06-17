afb48a06098bb58098ad08240d02896be40017aa
MiroslavaAutumn <miroslavaautumn@gmail.com>
2021-06-17 18:11:46 +0300

    added class SetUppedTestCase

--- options.py
+++ options.py
@@ -1,1 +1,16 @@
 chromedriver_path = "C:\MA\PY\Automations\duc_autotest\drivers\chrome\chromedriver.exe"
+url = 'https://ducatuswallet.rocknblock.io/'
+#url = "https://wallet.dicatus.io/"
+
+class SetUppedTestCase(unittest.TestCase):
+    def setUp(self):
+        print("Запуск!")
+        start_loading_time = time.time()
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
+        self.driver.implicitly_wait(1000)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+        self.driver.get(url=url)
+        end_loading_time = time.time()
+        loading_time = round(end_loading_time - start_loading_time, 2)
+        print(f"Page loaded in {loading_time} s")
